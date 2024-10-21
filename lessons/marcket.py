53a39f1e53887a313e9aa4d94c65b457b3864745
Haladilnik <goddov@gmail.com>
2024-10-21 15:27:21 +0300

    Market lab

--- lessons/marcket.py
+++ lessons/marcket.py
@@ -5,6 +5,6 @@ t=int(input("How many tomatos:"))
 cola=int(input("How many cola:"))
 chiken=int(input("How many chiken(KG):"))
 sum=c*3+t*2+cola*5+chiken*5
-print("Before tax: "+str(sum)+"\nAfter tax: "+str(sum+sum*17/100)+"\nTax:"+str(sum*17/100))
+print("Before tax: "+str(sum)+"\nAfter tax: "+str("%.2f" % (sum*1.17))+"\nTax:"+str("%.2f" % (sum*1.17-sum)))
 
 
