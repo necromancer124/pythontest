34ecb1e3cbecb3ebc6a436bda7eb73805f4848aa
Haladilnik <goddov@gmail.com>
2024-10-21 14:52:48 +0300

    v1

--- lessons/two.py
+++ lessons/two.py
@@ -1,6 +1,14 @@
-l="log in pls"
-print(l)
-mail=input("enter your email: ")
-age=input("enter your age: ")
-name=input("enter your name: ")
-print("Your mail: "+mail+"Your name: \n"+name+"Your age: \n"+age)
+# l="log in pls"
+# print(l)
+# mail=input("enter your email: ")
+# age=input("enter your age: ")
+# name=input("enter your name: ")
+# print("Your mail: "+mail+"Your name: \n"+name+"Your age: \n"+age)
+
+
+#task one
+
+num=input("4 digit number: ")
+if(len(num)==4):
+ num=int(num)
+ print("thousands:"+str(num//1000)+"\nhunderds:"+str(num//100%10)+"\ntens:"+str(num//10%10)+"\nlast num:"+str(num%10))
