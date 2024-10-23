d00eebce1a2db9fbf4e89afbd53851fb8616dde6
Haladilnik <goddov@gmail.com>
2024-10-23 12:10:50 +0300

    List test2

--- lessons/List.py
+++ lessons/List.py
@@ -1,6 +1,15 @@
-List={"name: "+input("name:"),"\nage: "+input("age:"),"\nmail: "+input("mail:")}
+List= [
+ "name: "+input("name:"),
+ "\nage: "+input("age:"),
+ "\nmail: "+input("mail:")
+]
 print(' '.join(List))
-List={input("id one:"),input("id two:")}
+
+
+
+List=[input("id one:"),input("id two:")]
 for i in range(3):
- List.add(input("id"+str(i+3)+":"))
+ List.append(input("id"+str(i+3)+":"))
  print(List)
+List.pop(2)
+print(List)
