1dbed0e0ede4b64c4e7d40257a2f9082937924d6
Haladilnik <goddov@gmail.com>
2024-11-12 11:36:16 +0200

    file test 1 def test

--- lessons/MyFileReader.py
+++ lessons/MyFileReader.py
@@ -1,4 +1,17 @@
+from time import sleep
+
 f = "C:\\Users\\nir\\PycharmProjects\\Tests\\lessons\\MyFile.txt"
+file=open(f,"r+")
+print(file.read())
+file.write("\nhelp\nagain")
+sleep(2)
+file.close
 file=open(f,"r")
 print(file.read())
 file.close
+file=open(f,"a")
+file.write("\nhelp\nagain")
+file.close()
+file=open(f,"r")
+print(file.read())
+file.close()
